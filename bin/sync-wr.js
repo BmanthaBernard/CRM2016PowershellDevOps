a36a430c5839cbb07d1118b398644338e6a50f47
Daniel Hines <dhines@regent.edu>
2017-05-17 15:06:40 -0400

    Still working on the path issue...

--- bin/sync-wr.js
+++ bin/sync-wr.js
@@ -1,21 +1,27 @@
 #! /usr/bin/env node
 var chalk = require('chalk')
+var binPath = require('bin-path')(require)
 var Shell = require('node-powershell')
-var path = process.argv.slice(2)[0]
-console.log(chalk.yellow('Syncing folder specified in ' + path))
-var ps = new Shell({
-  executionPolicy: 'Bypass',
-  noProfile: true
-})
 
-ps.addCommand('import-module Sync-WebResources.psm1')
-ps.addCommand('Sync-WebResources ' + path)
-ps.invoke()
-  .then(output => {
-    console.log(output)
-    ps.dispose()
-  })
-  .catch(err => {
-    console.log(err)
-    ps.dispose()
-  })
+var configPath = process.argv[3]
+
+var syncPath = binPath('sync-webresources')
+console.log(syncPath)
+
+// console.log(chalk.yellow('Syncing folder specified in ' + configPath))
+// var ps = new Shell({
+//   executionPolicy: 'Bypass',
+//   noProfile: true
+// })
+// var modPath = 
+// ps.addCommand('import-module Sync-WebResources.psm1')
+// ps.addCommand('Sync-WebResources ' + configPath)
+// ps.invoke()
+//   .then(output => {
+//     console.log(output)
+//     ps.dispose()
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     ps.dispose()
+//   })
